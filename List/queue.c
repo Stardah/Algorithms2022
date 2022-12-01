d5f4cc0e6d958abf5e01a84b348e04b297d36fbe
Adrax <terlychnikita@gmail.com>
2022-12-01 13:35:44 +0300

    List 2/2

---
+++ List/queue.c
@@ -0,0 +1,44 @@
+#include "list.h"
+#include "list.c"
+#include <stdlib.h>
+#include <stdio.h>
+
+int_list init_queue()
+{
+    int_list list = {
+        .front = NULL,
+        .back = NULL,
+    }
+
+    return list;
+}
+
+int queue_back(int_list *list)
+{
+    return list->back;
+}
+
+int queue_front(int_list *)
+{
+    return list->front;
+}
+
+void queue_push_back(int_list *list, int value)
+{
+    list_push_back(list, value);
+}
+
+void queue_pop_front(int_list *list)
+{
+    list_pop_front(list);
+}
+
+void queue_free(int_list * list)
+{
+    list_free(list);
+}
+
+/* int main(int argc, char const *argv[])
+{
+    return 0;
+} */
